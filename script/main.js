9ed41758bb8338e582ec17f53e4317d8939ee4ce
Androff <rageclock23@gmail.com>
2025-05-30 19:40:30 +0200

    feat: added new styles and fix some bugs

--- script/main.js
+++ script/main.js
@@ -1,11 +1,17 @@
 import chars from "./collection.js";
 
-const input = document.querySelector("input");
+const input = document.querySelector("#input-text");
 const output = document.querySelector(".output-text");
-const button = document.querySelector(".button-accept");
+const button = document.querySelector(".button__accept");
+const translate = document.querySelector(".translate_text");
+const clearText = document.querySelector(".button__clear");
 
 function showText() {
   let text = input.value;
+
+  if (text.length === 0) {
+    return (output.textContent = "....");
+  }
 
   const res = text
     .toUpperCase()
@@ -23,6 +29,12 @@ function showText() {
 }
 
 input.addEventListener("input", showText);
+
 button.addEventListener("click", () => {
   navigator.clipboard.writeText(output.textContent);
+});
+
+clearText.addEventListener("click", () => {
+  input.value = "";
+  showText();
 });
